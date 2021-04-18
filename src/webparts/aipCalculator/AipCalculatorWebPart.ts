7ae4da6e1d317024fb64664662aec87a5e955f5e
Pranav A. Kothare <pkothare@easydynamics.com>
2021-04-17 20:58:28 -0400

    Baseline commit.
    Generated the SPFx project using the generator [Build your first SharePoint client-side web part (Hello World part 1)](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/web-parts/get-started/build-a-hello-world-web-part). Provided values that are relevant to this project as follows.
    
    - What is your solution name?: hr-aip-calculator
    - Which baseline packages do you want to target for your component(s)?: SharePoint Online only (latest)
    - Where do you want to place the files?: Use the current folder
    - Do you want to allow the tenant admin the choice of being able to deploy the solution to all sites immediately without running any feature deployment or adding apps in sites?: N
    - Will the components in the solution require permissions to access web APIs that are unique and not shared with other components in the tenant?: Y
    - Which type of client-side component to create?: WebPart
    - What is your Web part name?: AIP Calculator
    - What is your Web part description?: Calculate potential payout based on role, individual performance and company performance
    - Which framework would you like to use?: React

---
+++ src/webparts/aipCalculator/AipCalculatorWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'AipCalculatorWebPartStrings';
+import AipCalculator from './components/AipCalculator';
+import { IAipCalculatorProps } from './components/IAipCalculatorProps';
+
+export interface IAipCalculatorWebPartProps {
+  description: string;
+}
+
+export default class AipCalculatorWebPart extends BaseClientSideWebPart<IAipCalculatorWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IAipCalculatorProps> = React.createElement(
+      AipCalculator,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/aipCalculator/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAipCalculatorWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AipCalculatorWebPartStrings' {
+  const strings: IAipCalculatorWebPartStrings;
+  export = strings;
+}
