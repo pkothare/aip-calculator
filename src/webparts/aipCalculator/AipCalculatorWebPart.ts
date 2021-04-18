df437106be808c4ff408ba226e08f46b512d9cd3
Pranav A. Kothare <pkothare@easydynamics.com>
2021-04-18 04:06:33 -0400

    AIP Calculator Webpart Settings (#2)
    * Add new localization strings for properties.
    
    Added new localization strings for the the webpart properties flyout
    when it is being configured. The strings that were added were:
    - RolesFieldLabel: The heading for the roles field
    - RolesFieldDescription: The description of the roles field
    - IndividualPerformanceBandsFieldLabel: The heading of the individual
      performance bands field
    - IndividualPerformanceBandsFieldDescription: The description of the
      individual performance bands field
    - CompanyPerformanceBandsFieldLabel: The heading of the company
      performance bands field
    - CompanyPerformanceBandsFieldDescription: The description of the
      company performance bands field
    
    * Add classes to represent dropdown options.
    
    Added `AipDropdown` as a base class to represent a dropdown option with
    the preset default values.
    
    Added `RoleDropdownOption` to represent a dropdown option for roles.
    Added `CompanyPerformanceBandDropdownOption` to represent a dropdown
    option for company performance bands. Added
    `IndividualPerformanceBandDropdownOption` to represent a dropdown option
    for individual performance bands. All these classes inherit from
    `AipDropdown`.
    
    * Update webpart properties to show new fields.
    
    Added the ability to modify the webpart the newly created properties:
    - `roles`
    - `individualPerformanceBands`
    - `companyPerformanceBands`
    These are multiline text fields that will contain CSV like data to be
    then transformed into dropdown options for the end user.
    
    * Add createDropDownOptions method.
    
    The createDropDownOptions method will allow the CSV like data entered in
    the webpart properties to be transformed into `AipDropdownOption`
    objects.
    
    * Add members to IAipCalculatorProps.ts
    
    This commit adds members to `IAipCalculatorProps` and also injects the
    `AipDrowdownOption` objects via the `render` method in
    `AipCalculatorWebPart`. The `AipCalculatorWebPart` render method was
    included to maintain the integrity of the build.

--- src/webparts/aipCalculator/AipCalculatorWebPart.ts
+++ src/webparts/aipCalculator/AipCalculatorWebPart.ts
@@ -10,21 +10,44 @@ import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import * as strings from 'AipCalculatorWebPartStrings';
 import AipCalculator from './components/AipCalculator';
 import { IAipCalculatorProps } from './components/IAipCalculatorProps';
+import RoleDropdownOption from './RoleDropdownOption';
+import AipDropdownOption from './AipDropdownOption';
+import IndividualPerformanceBandDropdownOption from './IndividualPerformanceBandDropdownOption';
+import CompanyPerformanceBandDropdownOption from './CompanyPerformanceBandDropdownOption';
 
 export interface IAipCalculatorWebPartProps {
   description: string;
+  roles: string;
+  individualPerformanceBands: string;
+  companyPerformanceBands: string;
 }
 
 export default class AipCalculatorWebPart extends BaseClientSideWebPart<IAipCalculatorWebPartProps> {
+
+  private createDropDownOptions<T extends AipDropdownOption>(
+    ctor: new (datatText: string) => T,
+    propertyValue: string): T[] {
+
+      if(!propertyValue) return null;
+      const dataTextCollection = propertyValue.split('\n');
+      let dropDownOptions = [];
+      for (var i = 0; i < dataTextCollection.length; i++) {
+        dropDownOptions.push(new ctor(dataTextCollection[i]));
+      }
+      return dropDownOptions;
+  }
 
   public render(): void {
     const element: React.ReactElement<IAipCalculatorProps> = React.createElement(
       AipCalculator,
       {
-        description: this.properties.description
+        description: this.properties.description,
+        roles: this.createDropDownOptions(RoleDropdownOption, this.properties.roles),
+        individualPerformanceBands: this.createDropDownOptions(IndividualPerformanceBandDropdownOption, this.properties.individualPerformanceBands),
+        companyPerformanceBands: this.createDropDownOptions(CompanyPerformanceBandDropdownOption, this.properties.companyPerformanceBands),
       }
     );
-
+    
     ReactDom.render(element, this.domElement);
   }
 
@@ -49,7 +72,28 @@ export default class AipCalculatorWebPart extends BaseClientSideWebPart<IAipCalc
               groupFields: [
                 PropertyPaneTextField('description', {
                   label: strings.DescriptionFieldLabel
-                })
+                }),
+                PropertyPaneTextField('roles', {
+                  label: strings.RolesFieldLabel,
+                  description: strings.RolesFieldDescription,
+                  multiline: true,
+                  resizable: false,
+                  rows: 10
+                }),
+                PropertyPaneTextField('individualPerformanceBands', {
+                  label: strings.IndividualPerformanceBandsFieldLabel,
+                  description: strings.IndividualPerformanceBandsFieldDescription,
+                  multiline: true,
+                  resizable: false,
+                  rows: 10
+                }),
+                PropertyPaneTextField('companyPerformanceBands', {
+                  label: strings.CompanyPerformanceBandsFieldLabel,
+                  description: strings.CompanyPerformanceBandsFieldDescription,
+                  multiline: true,
+                  resizable: false,
+                  rows: 10
+                }),
               ]
             }
           ]
--- src/webparts/aipCalculator/loc/en-us.js
+++ src/webparts/aipCalculator/loc/en-us.js
@@ -1,7 +1,13 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
+    "PropertyPaneDescription": "Configure the annual incentive plan calculator with your different settings",
     "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field"
+    "DescriptionFieldLabel": "Description Field",
+    "RolesFieldLabel": "Roles roles that users can choose from",
+    "RolesFieldDescription": "Enter only one role per line. Seprate the role name and percentage increment by a comma. For example: Senior Manager, 10",
+    "IndividualPerformanceBandsFieldLabel": "Individual performance bands that users can choose from",
+    "IndividualPerformanceBandsFieldDescription": "Enter only one band per line. The min value is inclusive and max value is exclusive. Separate the band name, min, max and percentage increment by commas. For example: Meets Expectations, 2.75, 3.25, 20",
+    "CompanyPerformanceBandsFieldLabel": "Company performance bands that users can choose from",
+    "CompanyPerformanceBandsFieldDescription": "Enter only one band per line. The min value is inclusive and max value is exclusive. Separate the band name, min, max and percentage increment by commas. For example: Stretch Goal, 8, 100, 100"
   }
 });
